ca0ddf9a090dd57457b62e4ae842c7d2cc8fbc18
tranhuynhnhattan96@gmail.com <tranhuynhnhattan96@gmail.com>
2024-05-15 00:40:12 +0700

    Day 4

--- src/components/day3/useReducer/counterUseReducer.jsx
+++ src/components/day3/useReducer/counterUseReducer.jsx
@@ -3,24 +3,39 @@ import { useReducer } from "react";
 const CounterUseReducer = () => {
   const [state, dispatch] = useReducer(
     (state, action) => {
-      console.log(state,state)
+      console.log(state, state);
       // if (action === "INCR") value += 1;
       // if (action === "DECR") value -= 1;
       // return value;
 
-      if (action === "INCR") state.value += 1;
-      if (action === "DECR") state.value -= 1;
-      // return { ...state };
-      return state
+      // if (action === "INCR") state.value += 1;
+      // if (action === "DECR") state.value -= 1;
+
+      // if (action.type === "INCR") state.value += 1;
+      // if (action.type === "DECR") state.value -= 1;
+
+      if (action.type === "INCR") state.value += action.value;
+      if (action.type === "DECR") state.value -= action.value;
+
+      return { ...state };
+      // return state DO NOT USE THIS
     },
     { value: 0 }
   );
 
   function incrValue() {
-    dispatch("INCR");
+    // dispatch("INCR");
+
+    // dispatch({type: "INCR"})
+
+    dispatch({ type: "INCR", value: 10 });
   }
   function decrValue() {
-    dispatch("DECR");
+    // dispatch("DECR");
+
+    // dispatch({ type: "DECR" });
+
+    dispatch({ type: "DECR", value: 10 });
   }
 
   return (
